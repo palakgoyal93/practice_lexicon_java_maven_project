cae7d94e39b902f8b4cb9506b9c2a7e50722b280
Palak Goyal <palakgoyal083@gmail.com>
2023-07-06 16:22:27 +0200

    Referred to the main class to repeat the code.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,23 @@
 package com.example;
 
 
+import java.util.Scanner;
+
 public class App {
 
-    import java.util.Scanner;
 
     public static void main(String[] args) {
-System.out.println("Welcome to the simple calculator application");
-System.out.println("Follow the steps to use it.");
+        System.out.println("Welcome to the simple calculator application");
+        System.out.println("Follow the steps to use it.");
 
-char operator;
-double number1, number2, result;
+        double number1, number2, result;
 
-// create an object of scanner class
-        scanner input = new scanner(System.in);
+        // create an object of scanner class
+        Scanner input = new Scanner(System.in);
 
- // ask users to enter operator
- System.out.println("Choose an operator: +, -, *, or /");
+        // ask users to enter operator
+        System.out.println("Choose an operator: +, -, *, or /");
+        char operator;
         operator = input.next().charAt(0);
 
         // ask users to enter numbers
@@ -26,40 +27,108 @@ double number1, number2, result;
         System.out.println("Enter second number");
         number2 = input.nextDouble();
 
+
         switch (operator) {
 
             // performs addition between numbers
-            case '+':
+            case '+'->{
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
-                break;
+            }
 
             // performs subtraction between numbers
-            case '-':
+            case '-'->{
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
-                break;
+            }
 
-            // performs multiplication between numbers
-            case '*':
+            case '*'->{
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
-                break;
 
-            // performs division between numbers
-            case '/':
+                // performs division between numbers
+            }
+
+            case '/'->{
                 result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
-                break;
+            }
+            // performs multiplication between numbers
+        }
 
-            default:
-                System.out.println("Invalid operator!");
-                break;
+
+
+        System.out.println("Do you want to continue ? \nPress y to continue else press n to exit");
+        char answer;
+        answer=input.next().charAt(0);
+        if (answer=='y'){
+            main(new String[]{});
+        }else {
+            input.close();
+        }
+    }
+
+    /*private static void calculate(){
+        System.out.println("Welcome to the simple calculator application");
+        System.out.println("Follow the steps to use it.");
+
+        double number1, number2, result;
+
+        // create an object of scanner class
+        Scanner input = new Scanner(System.in);
+
+        // ask users to enter operator
+        System.out.println("Choose an operator: +, -, *, or /");
+        char operator;
+        operator = input.next().charAt(0);
+
+        // ask users to enter numbers
+        System.out.println("Enter first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = input.nextDouble();
+
+
+        switch (operator) {
+
+            // performs addition between numbers
+            case '+'->{
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+            }
+
+            // performs subtraction between numbers
+            case '-'->{
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+            }
+
+            case '*'->{
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+
+                // performs division between numbers
+            }
+
+            case '/'->{
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+            }
+            // performs multiplication between numbers
         }
 
         input.close();
 
-System.out.println("Enter first number");
-    }
+        System.out.println("Do you want to continue ? \nPress y to continue else press n to exit");
+        char answer;
+        answer=input.next().charAt(0);
+        if (answer=='y'){
+            calculate();
+
+        }
+
+    }*/
 }
+
 
